cdcbc3c8ec7ca5d9f481bc0f16d5814f286a5587
nhng5827 <nhng5827@colorado.edu>
2018-04-22 22:41:21 -0600

    test details link4

--- app/details.js
+++ app/details.js
@@ -5,6 +5,8 @@ $(document).ready(function() {
 function myFunction(){
   var param1 = getParamValue('city');
   console.log("iuoihoh",param1)
+  url = "/details?city="+param1+".html"
+  $('#myIframe2').attr('src', url);
   window.location.href = "/details?city="+param1+".html";
   // window.top.location.href ="details.html";
 }
