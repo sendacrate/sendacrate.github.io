99baadca5a79aed3e2a6b1b5cdcd4fddb76027f4
nhng5827 <nhng5827@colorado.edu>
2018-04-23 13:43:29 -0600

    add other flight details

--- app/details.js
+++ app/details.js
@@ -14,9 +14,21 @@ $(document).ready(function() {
         console.log(data.bookings[i]['flight-info'][0]['res-num'])
         resnum= data.bookings[i]['flight-info'][0]['res-num']
         flightnum = data.bookings[i]['flight-info'][0]['flight-num']
+        dep_airport= data.bookings[i]['flight-info'][0]['dep-air']
+        dep_dt = data.bookings[i]['flight-info'][0]['dep']
+        board_pos= data.bookings[i]['flight-info'][0]['pos']
+        terminal = data.bookings[i]['flight-info'][0]['terminal']
+        gate= data.bookings[i]['flight-info'][0]['gate']
+        arr_airport = data.bookings[i]['flight-info'][0]['arr-air']
         if (param1 == city) {
             document.getElementById('res-num').innerHTML = resnum
             document.getElementById('flight-num').innerHTML = flightnum
+            document.getElementById('dep-air').innerHTML = dep_airport
+            document.getElementById('dep').innerHTML = dep_dt
+            document.getElementById('pos').innerHTML = board_pos
+            document.getElementById('terminal').innerHTML = terminal
+            document.getElementById('gate').innerHTML = gate
+            document.getElementById('arr-air').innerHTML = arr_airport
                 // booking_url = "booking.html?city="+name+"&param2=22"
                 // $('#myIframe').attr('src', booking_url);
         }
