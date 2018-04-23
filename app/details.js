e821ac7f185169885ad15abe63425b81a5e679a8
nhng5827 <nhng5827@colorado.edu>
2018-04-22 23:12:07 -0600

    test details link8

--- app/details.js
+++ app/details.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
 
-  var param1 = getParamValue('city');
-  console.log("iuoihoh",param1)
-  url = "/details.html?city="+param1
-  $('#myIframe2').attr('src', url);
-  document.getElementById('city').innerHTML = param1
+  // var param1 = getParamValue('city');
+  // console.log("iuoihoh",param1)
+  // url = "/app/details.html?city="+param1
+  // $('#myIframe2').attr('src', url);
+  // document.getElementById('city').innerHTML = param1
 })
 function myFunction(){
   var param1 = getParamValue('city');
-  url = "/details.html?city="+param1
+  url = "/app/details.html?city="+param1
   $('#myIframe2').attr('src', url);
   // window.location.href = "/app/details.html";
   window.top.location.href ="/app/details.html";
