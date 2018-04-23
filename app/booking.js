2bc50a6d63cc47b31ada0656f57cdda7c15e76d1
Nhi Nguyen <nhng5827@colorado.edu>
2018-04-22 21:05:42 -0600

    app initial push

---
+++ app/booking.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+    url = '/itin.json';
+    // console.log("hi")
+    var param1 = getParamValue('city');
+    $.getJSON(url, function(data) {
+        for(var i=0;i<data.bookings.length;i++){
+          city= data.bookings[i]['city']
+          date = data.bookings[i]['startDate']+"-"+data.bookings[i]['endDate']
+          if (param1 == city) {
+              document.getElementById('city').innerHTML = city
+              document.getElementById('date').innerHTML = date
+                  // booking_url = "booking.html?city="+name+"&param2=22"
+                  // $('#myIframe').attr('src', booking_url);
+              console.log(param1)
+          } else {
+              console.log("nah")
+        }
+        // city = data.bookings[1]['city']
+
+        console.log(city)
+
+        // console.log()
+        // console.log("hi")
+
+
+        // console.log(param1)
+
+        }
+    });
+})
---
+++ app/getSchedule.js
@@ -0,0 +1,44 @@
+$(document).ready (function(){
+  url = '/itin.json';
+  // console.log("hi")
+
+  $.getJSON(url, function(data) {
+      for(var i=0;i<data.bookings.length;i++){
+        city= data.bookings[i]['city']
+        booking_url = "booking.html?city="+city
+        var iframe = document.createElement('iframe');
+        iframe.frameBorder=0;
+        iframe.width="250px";
+        iframe.height="250px";
+        iframe.id=city;
+        // booking_url = "booking.html?city="+"Boston"
+        iframe.setAttribute("src", booking_url);
+        document.getElementById("container").appendChild(iframe);
+    }
+  });
+  name= "Seattle"
+
+
+//   var frames = document.getElementsByTagName('myIframe');
+//
+// for (var i = 0; i < frames.length; i++){
+//
+//     fSrc = frames[i].src;
+//
+// }
+// for(var i=0;i<3;i++){
+$('#myIframe').attr('src', booking_url);
+
+  // console.log(param1)
+})
+function getParamValue(paramName)
+{
+    var url = window.location.search.substring(1); //get rid of "?" in querystring
+    var qArray = url.split('&'); //get key-value pairs
+    for (var i = 0; i < qArray.length; i++)
+    {
+        var pArr = qArray[i].split('='); //split key and value
+        if (pArr[0] == paramName)
+            return pArr[1]; //return value
+    }
+}
