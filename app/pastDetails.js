2be504985a266ea3e1b3d68135823c96fbb7639f
nhng5827 <nhng5827@colorado.edu>
2018-04-23 23:18:34 -0600

    past trips12

---
+++ app/pastDetails.js
@@ -0,0 +1,69 @@
+$(document).ready(function() {
+
+  var param1 = getParamValue('city');
+  // console.log("iuoihoh",param1)
+  url = "/app/pastDetails.html?city="+param1
+  $('#myIframe2').attr('src', url);
+  // document.getElementById('city').innerHTML = param1
+  url = '/app/itin.json';
+  // console.log("hi")
+  var today = new Date();
+  var dd = today.getDate();
+  var mm = today.getMonth()+1; //January is 0!
+  var yy = String(today.getFullYear()).substring(2,4);
+
+  if(dd<10) {
+      dd = '0'+dd
+  }
+
+  if(mm<10) {
+      mm = '0'+mm
+  }
+
+  today = new Date(mm + '/' + dd + '/' + yy);
+  // console.log(today);
+  $.getJSON(url, function(data) {
+      for(var i=0;i<data.bookings.length;i++){
+        start_dt = new Date(data.bookings[i]['startDate']);
+        // console.log(start_dt);
+        if (start_dt<=today){
+
+
+        date = data.bookings[i]['startDate']+"-"+data.bookings[i]['endDate']
+        city= data.bookings[i]['city']
+        console.log(data.bookings[i]['flight-info'][0]['res-num'])
+        resnum= data.bookings[i]['flight-info'][0]['res-num']
+        flightnum = data.bookings[i]['flight-info'][0]['flight-num']
+        dep_airport= data.bookings[i]['flight-info'][0]['from']
+        dep_dt = data.bookings[i]['flight-info'][0]['dep']
+        board_pos= data.bookings[i]['flight-info'][0]['pos']
+        terminal = data.bookings[i]['flight-info'][0]['terminal']
+        gate= data.bookings[i]['flight-info'][0]['gate']
+        arr_airport = data.bookings[i]['flight-info'][0]['to']
+        arr_dt = data.bookings[i]['flight-info'][0]['arr']
+        if (param1 == city) {
+            document.getElementById('res-num').innerHTML = resnum
+            document.getElementById('flight-num').innerHTML = flightnum
+            document.getElementById('dep-air').innerHTML = dep_airport
+            document.getElementById('dep').innerHTML = dep_dt
+            document.getElementById('pos').innerHTML = board_pos
+            document.getElementById('terminal').innerHTML = terminal
+            document.getElementById('gate').innerHTML = gate
+            document.getElementById('arr').innerHTML = arr_dt
+            document.getElementById('arr-air').innerHTML = arr_airport
+            document.getElementById('city-det').innerHTML = param1+", "+date
+                // booking_url = "booking.html?city="+name+"&param2=22"
+                // $('#myIframe').attr('src', booking_url);
+        }
+
+      }
+      }
+    })
+})
+function myFunction(){
+  var param1 = getParamValue('city');
+  // url = "/app/details.html?city="+param1
+  // $('#myIframe2').attr('src', url);
+  // window.location.href = "/app/details.html";
+  window.top.location.href ="/app/pastDetails.html?city="+param1;
+}
